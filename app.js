1ec93f3f112f9d5543a0fb2cc8114349c4a7e451
Youssef ElMessiry <youssef.elmessiry92@gmail.com>
2019-05-05 08:32:34 +0000

    remove comments

--- app.js
+++ app.js
@@ -43,36 +43,6 @@ app.use(function(req, res, next){
    next();
 });
 
-
-// var campgroundSchema = new mongoose.Schema({
-//   name: String,
-//   image: String,
-//   description: String 
-// });
-
-// var Campground = mongoose.model("Campground",campgroundSchema);
-
-// var campgrounds = [
-//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-// ];
-
-
-
-// ============ COMMENTS ROUTES ============
-
-
-
-// ============ AUTHENTICATION ROUTES ============
-
-
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
